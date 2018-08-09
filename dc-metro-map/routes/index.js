294c493e5d8b5ed33ee59a22ff61e280d3dc97a0
aberkow <berkowitz.clarinet@gmail.com>
2018-08-09 13:48:10 -0400

    test

--- dc-metro-map/routes/index.js
+++ dc-metro-map/routes/index.js
@@ -33,7 +33,7 @@ if (RAINBOW == 'true') { console.log("Rainbows! "); }
 //-----------------------------------------------------------------------------
 function wmataJsonToGeoJson(jsonData) {
   var dataOut = { type: "FeatureCollection", features: [] };
-  console.log(dataOut);
+  // console.log(dataOut);
   var markerSym = "bus";
   var markerColor = "#000000";
   if (BEERME == 'true') { markerSym = "beer"; }
